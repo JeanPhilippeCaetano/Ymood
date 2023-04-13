1087edc15caa923c2aacc9d13efbb361bd5822da
lbastien <bastienlocat@gmail.com>
2023-04-13 19:21:08 +0200

    debut de la copie du Yextranet

--- ymood/src/App.js
+++ ymood/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import img from './logo_ynov_campus_rvb.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="conteneur">
+      <nav className="navtop">
+        <img id="Ymage" src={img} />
+        <ul id="Yul">
+          <div>fr</div>
+          <div>en</div>
+          <div>Ymail@ynov.com</div>
+        </ul>
+      </nav>
+      <div className="Ycloud">JE SUIS LE YCLOUD</div>
+      <div className="Ydoc">
+        <button>Tout</button>
+        <button>Scolarité</button>
+        <button>Promotions et partenariats</button>
+        <button>Documents</button>
+      </div>
+      <div className="Ybiggrid">
+        <div className="Yhypperplan"><div className="texth">Hyperplanning</div></div>
+        <div className="Ydossier"></div>
+        <div className="Ymatch"></div>
+        <div className="YoffreAuto"></div>
+        <div className="Yprof"></div>
+        <div className="YoffrePc"></div>
+        <div className="Yisic"></div>
+        <div className="Ymater"></div>
+        <div className="Ymicro"></div>
+        <div className="Yammer"></div>
+        <div className="Ymail"></div>
+        <div className="Yreglement"></div>
+        <div className="Ylearning"></div>
+        <div className="Ypeda"></div>
+        <div className="Ybrand"></div>
+        <div className="Yadobe"></div>
+        <div className="Ychart"></div>
+        <div className="Ysoftware"></div>
+        <div className="Ygit"></div>
+        <div className="Ydoss"></div>
+        <div className="Yfisio"></div>
+      </div>
     </div>
+    
   );
 }
 

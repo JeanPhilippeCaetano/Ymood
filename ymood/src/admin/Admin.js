de2e6a395bee486aa24a5d405813aaa794b71c84
unknown <hugo.roumagne@ynov.com>
2023-04-13 15:40:16 +0200

    add router nav

---
+++ ymood/src/admin/Admin.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+function Admin() {
+
+  return (
+    <div>
+      <h1>Page d'administration</h1>
+    </div>
+  );
+}
+
+export default Admin;

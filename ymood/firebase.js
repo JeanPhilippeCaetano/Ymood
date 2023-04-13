05ef0b9595ad9db57c44d6b98441d44090cbadf2
Spikesito <94541386+Spikesito@users.noreply.github.com>
2023-04-13 12:26:39 +0200

    Init project

---
+++ ymood/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAL6Zqx5f5D1HNXBlo7cAEQbw_GTpBgSNM",
+  authDomain: "ymood-ce910.firebaseapp.com",
+  databaseURL: "https://ymood-ce910-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "ymood-ce910",
+  storageBucket: "ymood-ce910.appspot.com",
+  messagingSenderId: "116824563922",
+  appId: "1:116824563922:web:95e868566a807fee018d0b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

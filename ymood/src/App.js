06df77c60ab95300ad7915a85867d1d3cef6091a
JeanPhilippeCaetano <jp@LAPTOP-4451E3BE.localdomain>
2023-04-14 09:52:54 +0200

    fix bugs from webapp-back-nosql

--- ymood/src/App.js
+++ ymood/src/App.js
@@ -5,14 +5,12 @@ import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import AuthDetails from './components/AuthDetails';
 import AddEmotions from './components/AddEmotions';
-import LoginPage from './pages/LoginPage.js';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/loginregister" element={<LoginPage />} />
         <Route path="/extranet" element = {<Extranet />} />
       </Routes>
     

47e238711168f545be89ed7a10d3d0ee699b36fe
JeanPhilippeCaetano <jp@LAPTOP-4451E3BE.localdomain>
2023-04-14 21:58:01 +0200

    buttons add value done

--- ymood/src/components/AddEmotions.js
+++ ymood/src/components/AddEmotions.js
@@ -4,7 +4,6 @@ import { onAuthStateChanged } from "firebase/auth";
 import { ref, get, child, push, set } from "firebase/database";
 import { collection, getDocs } from "firebase/firestore";
 import EmotionsCrud from "../EmotionsCrud.js";
-import { useParams } from "react-router-dom";
 
 
 const emotions = {
@@ -20,31 +19,16 @@ const emotions = {
     10 : { name : "soulagé", isPositive: true},
 }
 
-const AddEmotion = () => {
-    let id = useParams()
-    addValue(id)
-}
+const AddEmotions = () => {
+    const [authUser, setAuthUser] = useState(null);
+    const [emotion, setEmotion] = useState({ name: "", isPositive: true });
 
-async function addValue(id) {
-    const t = new EmotionsCrud()
-    // const count = await t.readData(`/values/${year}/${month}/${day}/1`);
     let now = new Date()
     const year = now.getFullYear();
     const month = now.getMonth() + 1;
     const day = now.getDate();
-    const count = t.readData(`/values/${year}/${month}/${day}/${id}`);
-    console.log(count)
-    // let count = (isNaN(t.readData(`/values/${year}/${month}/${day}/${id}`)) ? 0 : t.readData(`/values/${year}/${month}/${day}/${id}`))
-    t.updateEmotion(`/values/${year}/${month}/${day}/${id}`, count+1)
-}
 
-const AddEmotions = () => {
-    const [authUser, setAuthUser] = useState(null);
-    const [emotion, setEmotion] = useState(null);
 
-   
-
-    
 
     useEffect(() => {
         const listen = onAuthStateChanged(auth, (user) => {
@@ -59,38 +43,31 @@ const AddEmotions = () => {
         }
     }, [])
 
-    const initWeek = () => {
-        set(ref(db, `/${year}/${month}/${day}/`))
+
+    const addValue = async (event) => {
+        const t = new EmotionsCrud()
+        const id = parseInt(event.target.value)
+        const count = await t.readData(`/values/${year}/${month}/${day}/${id}`);
+
+        // let count = (isNaN(t.readData(`/values/${year}/${month}/${day}/${id}`)) ? 0 : t.readData(`/values/${year}/${month}/${day}/${id}`))
+        t.updateEmotion(`/values/${year}/${month}/${day}/${id}`, count+1)
+        console.log(count)
     }
-
-    // const initAdmins = () => {
-    //     let admins = {jp:"WDzweZbWbmM1WgnrCDsPL2X7w282",emile:"knKcXFBWX3Zduk3bXXZJNccQoHv2"}
-    //     set(ref(db, "/admins/"), admins)
-    // }
-
-    // const initEmotions = () => {
-    //     set(ref(db, "/emotions/"), emotions)
-    // }
-
-    
 
     return (
         <div> {authUser ? (
             <>
                 <p>{`Signed In as ${authUser.email}`}</p>
-                <form action="/add-emotion/">
-                <p>Select an emotion:</p>
-                <input type="submit" name="1" value="énervé"/>
-                <input type="submit" name="2" value="joyeux"/>
-                <input type="submit" name="3" value="triste"/>
-                <input type="submit" name="4" value="inquiet"/>
-                <input type="submit" name="5" value="serein"/>
-                <input type="submit" name="6" value="fatigué"/>
-                <input type="submit" name="7" value="déçu"/>
-                <input type="submit" name="8" value="excité"/>
-                <input type="submit" name="9" value="frustré"/>
-                <input type="submit" name="10" value="soulagé"/>
-                </form>
+                <button onClick={addValue} value="1"> Enervé </button>
+                <button onClick={addValue} value="2"> Joyeux </button>
+                <button onClick={addValue} value="3"> Triste </button>
+                <button onClick={addValue} value="4"> Inquiet </button>
+                <button onClick={addValue} value="5"> Serein </button>
+                <button onClick={addValue} value="6"> Fatigué </button>
+                <button onClick={addValue} value="7"> Déçu </button>
+                <button onClick={addValue} value="8"> Excité </button>
+                <button onClick={addValue} value="9"> Frustré </button>
+                <button onClick={addValue} value="10"> Soulagé </button>
             </>
         ) : (
             <p> Logged Out</p>
